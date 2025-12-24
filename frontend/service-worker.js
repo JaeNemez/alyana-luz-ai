2b764f9e9c47f90f82697e045727d39016abcc0e
JaeNemez <122192529+JaeNemez@users.noreply.github.com>
2025-12-24 17:09:15 -0500

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,9 +1,18 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
+
 
 
